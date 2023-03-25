a138c5621c07602b9e08fbb3b29873507bdab29d
Ian Marino <ian.marino.r@gmail.com>
2023-03-25 11:50:09 +0100

    - props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+
+let human = {
+  head: false,
+  armas: true,
+  nails: [1,2,3,5],
+  person: {
+    name: 'Ian',
+    surname: 'Marino'
+  }
+}
+
+const App = () => (
+  <div>
+    <Ian />
+
+ {/* here I pass an object to Maria component */}
+ {/* Maria component will then use this object to populate its HTML elements */}
+    <Maria human={human} />
+  </div>
+);
+
+
+
+class Ian extends React.Component {
+  nome = "maria";
+  site = (<h1>cruzes</h1>);
+
+  faz() {
+    alert("cu");
+  }
+
+  random() {
+    return Math.random() * 10;
+  }
+
+  render = () => {
+    return (
+      <div onClick={this.faz}>
+        <h2>O numero também nao é </h2>
+      </div>
+    );
+  };
+}
+
+
+let Maria = (props) => {
+
+  let _props = props
+
+  console.log(_props)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h2>Name: {props.human.person.name}</h2>
+      <h2>Surname: {props.human.person.surname}</h2>
+      <h3>Number of nails: {props.human.nails[3]}</h3>
+      <h3> {props.human.head ? 'ALIVE' : 'HEADLESS'}</h3>
+
+      
     </div>
   );
+
+
 }
 
 export default App;
---
+++ testando.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4]
+
+
+let newArr = arr.map((letra, i) => 
+    letra + 0
+    
+
+)
+console.log('arr: ', newArr);
